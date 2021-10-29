c9d12e7199e2668bc06a560ac2ec9fdb671f5c54
dulanmalinda <dulanmalinda@gmail.com>
2021-10-29 18:48:23 +0530

    initial

---
+++ app/config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    url: "mongodb+srv://dewmin:1234@dewmindb.whjvz.mongodb.net/test",
+
+    stockUrl: "https://gneeferserver.herokuapp.com/api/stock/",
+
+
+    stocLogkUrl: "https://gneeferserver.herokuapp.com/api/stockLog/",
+
+    purchaseUrl: "https://gneeferserver.herokuapp.com/api/purchase/",
+
+    ItemNameUrl: "https://gneeferserver.herokuapp.com/api/item/namebyid"
+}
